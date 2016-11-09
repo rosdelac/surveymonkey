f13c3489e4ad1468e9d227b5d08f34356bebc895
codeamac07 <codeamac07@gmail.com>
2016-11-08 18:09:36 -0600

    graficas

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,4 +3,31 @@ class Survey < ActiveRecord::Base
   has_many :participations
   has_many :users, through: :participations
   belongs_to :user
+
+  def taken?
+    if self.participations.count > 0
+      true
+    else
+      false
+    end
+  end
+
+  def taken_times
+    self.participations.count
+  end
+
+  def get_options
+    if self.taken?
+      options = []
+      questions = self.questions
+      questions.each do |q|
+        q.options.each do |op|
+          options << op
+        end
+      end
+      options
+    else
+      nil
+    end
+  end
 end
